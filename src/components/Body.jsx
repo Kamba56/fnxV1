0929d95dd9772b886e494060e1dd8b3b07a1819f
Toma John Musa <tomajohn316@yahoo.com>
2023-12-03 18:17:29 +0100

    FIx card

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,32 +1,14 @@
-import Button from "./Button";
+import Card from "./Card";
 
 const HomeBody = () => {
   return (
-    <section className="m-auto py-28 px-7 max-w-[1440px] flex flex-col gap-16">
-      <h2 className="font-semibold text-4xl">How We Manage And Secure Your Investments </h2>
+    <section className="m-auto py-28 px-[5%] max-w-[1440px] flex flex-col gap-14">
+      <h2 data-aos="fade-right" className="font-semibold text-4xl">How We Manage And Secure Your Investments </h2>
 
-      <div className="flex flex-col gap-12 lg:flex-row lg:gap-20 lg:justify-between items-stretch">
-        <article data-aos="flip-right" data-aos-duration="1200" className="flex flex-col gap-7 flex-1 justify-between">
-          <div className="flex flex-col gap-7">
-            <h3 className="font-medium text-2xl border-b border-blue-50 w-fit">Expect more from your Management Company</h3>
-            <p className="text-lg">Demand a team which has real and direct experience of managing UCITS and AIF Funds, raising assets, and regulatory matters in the US, UK and Europe. Demand an agile firm which adapts to new customer demands efficiently and effectively</p>
-          </div>
-
-          <div data-aos="flip-up" data-aos-duration="2000" className="w-fit self-start">
-            <Button name="Our AIFMD Services" linkTo="services" color="white" />
-          </div>
-        </article>
-
-        <article data-aos="flip-right" data-aos-duration="1200" className="flex flex-col gap-7 flex-1 justify-between">
-          <div className="flex flex-col gap-7">
-            <h3 className="font-medium text-2xl border-b border-blue-50 w-fit">A partnership that extends beyond management services</h3>
-            <p className="text-lg">Work with a Partner with a background in investor Due Diligence, Fund Structuring, and Operations.</p>
-          </div>
-
-          <div data-aos="flip-down" data-aos-duration="2000" className="w-fit self-start">
-            <Button name="Our Values" linkTo="values" color="white" />
-          </div>
-        </article>
+      <div className="flex flex-wrap gap-4">
+        <Card title="Expect more from your Management Company" text="Demand a team which has real and direct experience of managing UCITS and AIF Funds, raising assets, and regulatory matters in the US, UK and Europe. Demand an agile firm which adapts to new customer demands efficiently and effectively" />
+        <Card title="The right Provider for your current business needs" text="Firms most often fail due to high fixed costs, take full advantage of our network of providers selected to offer the right service for each stage of the firm’s growth." />
+        <Card title="A partnership that extends beyond management services" text="Work with a Partner with a background in investor Due Diligence, Fund Structuring, and Operations." />
       </div>
     </section>
   )
---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+const Card = (props) => {
+  const { title, text } = props;
+  return (
+    <section className="flex p-6 bg-white flex-col gap-4 shadow-md lg:shadow-none lg:hover:shadow-md w-full mx-auto md:mx-0 h-full max-w-[400px] md:max-w-[300px] lg:max-w-[400px]">
+      <h3 data-aos="fade-right" className="font-medium text-2xl">{title}</h3>
+      <p data-aos="fade-right" className="font-normal text-lg">{text}</p>
+    </section>
+  );
+};
+
+export default Card;
