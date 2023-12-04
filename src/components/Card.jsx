769a3b331779b43ed11eac852c7c3ecff785d130
Toma John Musa <tomajohn316@yahoo.com>
2023-12-04 22:02:56 +0100

    Fix card bug

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 const Card = (props) => {
   const { title, text } = props;
   return (
-    <section className="flex p-6 bg-white flex-col gap-4 shadow-md lg:shadow-none lg:hover:shadow-md w-full mx-auto md:mx-0 max-w-[400px] md:max-w-[300px] lg:max-w-[400px]">
+    <section className="flex p-6 bg-white flex-col gap-4 shadow-md lg:shadow-none lg:hover:shadow-md w-full mx-auto md:mx-0 max-w-[400px] md:max-w-[350px] 2xl:max-w-[400px]">
       <h3 data-aos="fade-right" className="font-medium text-2xl">{title}</h3>
       <p data-aos="fade-right" className="font-normal text-lg">{text}</p>
     </section>
