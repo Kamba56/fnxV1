9f16eec73ffeca6f8a9c0210fc3b55d313e8f13d
Toma John Musa <tomajohn316@yahoo.com>
2023-12-12 22:27:58 +0100

    Fix image quality and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@ import logo from "../assets/images/log.svg";
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col lg:flex-row text-white font-light">
+    <footer className="flex flex-col lg:flex-row text-white font-light bg-blue-50">
 
-      <div className="flex-1 order-2 lg:order-1 pt-10 pb-12 px-[5%] flex flex-col items-start gap-8 bg-blue-50">
+      <div className="flex-1 order-2 lg:order-1 pt-10 pb-12 px-[5%] hidden lg:flex flex-col items-start gap-8 bg-blue-50">
         <Link to='/' className="w-16">
           <img src={logo} alt="Logo" />
         </Link>
@@ -15,7 +15,7 @@ const Footer = () => {
         <p className="font-light text-sm">©️ 2023 FNX Dublin Limited, All Rights Reserved.</p>
       </div>
 
-      <div className="flex-1 pt-10 pb-12 px-[5%] order-1 lg:order-2 flex flex-col items-start gap-8 bg-brown-50">
+      <div className="flex-1 pt-10 pb-12 px-[5%] order-1 lg:order-2 hidden lg:flex flex-col items-start gap-8 bg-brown-50">
         <div className="flex rounded-lg p-[2px] items-center w-full max-w-lg justify-between bg-white">
           <input type="email" placeholder="Your Email Address" name="" id="" className="bg-none pl-3 border-none outline-none w-full" />
           <button className="px-6 text-center flex items-center justify-center py-3 text-base font-normal rounded text-white hover:shadow-lg bg-blue-50">Subscribe</button>
@@ -31,6 +31,36 @@ const Footer = () => {
           <Link to='/about/privacy'>Privacy Policy</Link>
           <Link to='/about/sustainability'>SFDR  Disclosures</Link>
         </ul>
+      </div>
+
+      <div className="flex-1 pt-10 px-[5%] flex lg:hidden flex-col items-start gap-10">
+        <Link to='/' className="w-16">
+          <img src={logo} alt="Logo" />
+        </Link>
+
+        <div className="flex rounded-lg p-[2px] items-center w-full max-w-lg justify-between bg-white">
+          <input type="email" placeholder="Your Email Address" name="" id="" className="bg-none pl-3 border-none outline-none w-full" />
+          <button className="px-6 text-center flex items-center justify-center py-3 text-base font-normal rounded text-white hover:shadow-lg bg-brown-50">Subscribe</button>
+        </div>
+
+        <p className="text-blue-10 text-base">FNX Dublin Limited is registered in the Republic of Ireland with <span className="font-semibold text-brown-50">Company Number 682114 </span>. FNX Dublin Limited is authorised under AIFMD by Central Bank of Ireland with <span className="font-semibold text-brown-50">registration number C441669 </span>.</p>
+      </div>
+
+      <div className="flex-1 pt-10 pb-16 px-[5%] flex lg:hidden flex-col items-start gap-8">
+        
+
+        <h3 className="font-medium text-lg">Send us an email or give us your information and we wil get back to you.</h3>
+        <div className="w-fit">
+          <Link data-aos="fade-right" to='/contact' className={`px-12 text-center flex items-center justify-center py-3 text-base font-normal rounded text-white hover:shadow-lg bg-brown-50`}>Contact</Link>
+        </div>
+
+        <ul className="flex gap-3 justify-between font-medium underline text-sm text-center">
+          <NavLink to='/about/disclaimer'>Disclaimer</NavLink>
+          <Link to='/about/privacy'>Privacy Policy</Link>
+          <Link to='/about/sustainability'>SFDR  Disclosures</Link>
+        </ul>
+
+        <p className="font-light text-sm">©️ 2023 FNX Dublin Limited, All Rights Reserved.</p>
       </div>
 
 
