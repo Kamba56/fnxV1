2fde48be3f1694fbcfd767ebf4e254e4d5d52c0a
Toma John Musa <tomajohn316@yahoo.com>
2023-12-20 23:26:42 +0100

    Fix team page

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,14 +1,12 @@
-import { GrClose } from "react-icons/gr";
-import blacklogo from "../assets/images/blacklogo.svg"
-import avatar from "../assets/images/avatar.png";
+import { IoMdClose } from "react-icons/io";
 
 const Player = (props) => {
   const { member, closePop } = props;
 
   return (
-    <main data-aos="fade-down-left" className="fixed top-0 h-screen w-full text-white overflowy-auto bg-blue-50 px-[5%] flex items-center flex-col lg:flex-row text-center gap-8 lg:text-left lg:py-12 pt-20 z-[10000] cursor-close" onClick={closePop}>
-      <button onClick={closePop} className="absolute top-8 right-10 lg:hidden !text-white">
-          <GrClose size={30} className="!text-white" />
+    <main data-aos="fade-down-left" className="fixed top-0 h-screen w-full text-white overflowy-auto bg-blue-50 px-[5%] flex items-center flex-col lg:flex-row text-center gap-8 lg:text-left lg:py-12 pt-20 z-[10000]">
+      <button onClick={closePop} className="absolute top-8 right-10 !text-white">
+          <IoMdClose size={30} className="!text-white" />
         </button>
       <div className="flex flex-col gap-2 lg:w-1/4">
         <h3 className="font-medium text-xl">{member.name}</h3>
@@ -20,7 +18,9 @@ const Player = (props) => {
 
         <h3 className="font-medium text-xl">Contact {member.name.split(" ")[0]}</h3>
         <ul className="flex flex-col gap-3 text-base">
-          <li>{member.mail}</li>
+          <li>
+            <a href={`mailto:${member.mail}`}>{member.mail}</a>
+          </li>
           <li>
             <a href={member.linkedin} target="_blank" rel="noopener noreferrer" className="underline">LinkedIn</a>
           </li>
--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -24,7 +24,7 @@ const Team = () => {
       position: "Co-Founder",
       about: "Mark has been working in Financial Services since 1994 working in senior roles with various investors for Investment Selection and Due Diligence. Prior to FNX, he has overseen hundreds of managers and fund launches across most asset classes. He has acted as COO, Advisor and Non-Executive Director to multiple companies such as Blackstar Asset Management, AON and AXA Investment Managers. Mark is a Fellow of the Institute of Chartered Accountants in Ireland and a Chartered Alternative Investment Analyst.  ",
       mail: "mark.flanagan@fnx.ie",
-      linkedin: "#",
+      linkedin: "https://www.linkedin.com/in/nedirector/",
       image: mark
     },
     {
@@ -32,7 +32,7 @@ const Team = () => {
       position: "Co-Founder",
       about: "Andrew moved into Finance from Civil and Nautical Engineering in 1995. Over the subsequent 20 years Andrew worked for a number of Tier 1 institutions in which he developed and implemented Futures and Options strategies based on Technical and Statistical Methods. During the 2007 Credit Crisis Andrew managed the dismantling of a significant defaulting options positions on behalf of SocGen (Newedge). In 2010 Andrew co-founded Finex LLP and has grown the partnership into a UCITS and AIFM firm. In 2015 Andrew won the Technical Analyst Award. Andrew holds a BEng in Civil Engineering Design and Management from Cardiff University and holds certificates from the FCA in Client dealing and Partner functions.",
       mail: "andrew.gebhardt@fnx.ie",
-      linkedin: "",
+      linkedin: "https://www.linkedin.com/in/andrew-j-s-gebhardt-83513515/?originalSubdomain=uk",
       image: ""
     },
     {
@@ -40,20 +40,20 @@ const Team = () => {
       position: "Co-Founder",
       about: "Having begun his career in 1986 Mark has extensive experience in risk, compliance and operations specializing within both the Irish AIF and UCITS regimes. Mark has a comprehensive knowledge of investment risk and regulatory reporting under ESMA / Annex IV, Form PF and OPERA as well as financial reporting under US GAAP and IFRS. He is an Irish accountant and US CPA.  Mark has previously worked with South Quay Capital, Castille Capital, Aros Capital Management Ltd and Citco Mark holds a degree in Accounting and Finance from Dublin City University and is on the advisory board of the Centre for Talented Youth Ireland.",
       mail: "mark.fitzpatrick@fnx.ie",
-      linkedin: ""
+      linkedin: "https://www.linkedin.com/in/markfitzpatrickfca/"
     },
     {
       name: "Peter Forsey",
       position: "Co-Founder",
       about: "Peter began his career in 1995 and worked in financial services across high-street banking, commercial banking, asset management, alternative investments, middle-office, custodial and high-intensity front office equity trading. He has specialised within programme project management and business analysis to build, implement and oversee risk, reporting and trading systems. Peter holds a degree in Software Engineering Management and Diploma in Financial Services (UCD).  He is a licentiate member of the Institute of Bankers, holding a Certificate and Diploma in Investment Fund Services.",
       mail: "peter.forsey@fnx.ie",
-      linkedin: ""
+      linkedin: "https://www.linkedin.com/in/pforsey/"
     },
     {
       name: "Claire O’Doherty",
       position: "Co-Founder",
       about: "Claire began her career in 2011 and has acted as a Risk Officer for several credit unions within Ireland. Claire is the Chief Compliance Officer for FNX Dublin Ltd and is the Risk and Compliance Officer for Whelan Dowling & Associates and Red Flare. She holds a Bachelor of Commerce from University of Galway and is a Certified Chartered Accountant (ACCA), and holds a Diploma in Risk Management, Internal Audit and Compliance with the Institute of Chartered Accountants. ",
-      mail: "",
+      mail: "claire.ordoherty@fnx.ie",
       linkedin: ""
     }
   ];
@@ -66,7 +66,7 @@ const Team = () => {
 
       <div className="flex flex-col gap-10 items-center pt-32">
         <article className="max-w-[390px] min-w-72 w-full flex-col items-center shadow-md lg:shadow-none lg:hover:shadow-md gap-3 flex cursor-pointer pb-7" onClick={() => {handlePop(0)}}>
-          <figure className="w-full h-auto max-h-[258px] rounded-lg shadow-md overflow-hidden bg-transparent">
+          <figure className="w-full h-auto max-h-[320px] rounded-lg shadow-md overflow-hidden bg-transparent">
             {members[0].image ? <img src={members[0].image} alt="Avatar" /> : <img src={avatar} alt="Avatar" />}
           </figure>
 
