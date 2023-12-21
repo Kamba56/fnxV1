5c733717ab391c821ff4bded7ddd6d2f3e30f1ca
Toma John Musa <tomajohn316@yahoo.com>
2023-12-21 10:29:18 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,6 +16,11 @@ const Footer = () => {
         <p className="text-blue-10 text-sm">FNX Dublin Limited is registered in the Republic of Ireland with <span className="font-semibold text-brown-50">Company Number 682114 </span>. FNX Dublin Limited is authorised under AIFMD by Central Bank of Ireland with <span className="font-semibold text-brown-50">registration number C441669 </span>.</p>
 
         <p className="font-light text-xs">©️ 2023 FNX Dublin Limited, All Rights Reserved.</p>
+        <ul className="flex gap-3 justify-between font-medium underline text-sm text-center">
+          <NavLink to='/about/disclaimer'>Disclaimer</NavLink>
+          <Link to='/about/privacy'>Privacy Policy</Link>
+          <Link to='/about/sustainability'>SFDR  Disclosures</Link>
+        </ul>
       </div>
 
       <div className="flex-1 pt-10 pb-12 px-[5%] order-1 lg:order-2 hidden lg:flex flex-col items-start gap-8 bg-brown-50">
@@ -23,17 +28,7 @@ const Footer = () => {
           <input type="email" placeholder="Your Email Address" name="" id="" className="bg-none pl-3 border-none outline-none w-full" />
           <button className="px-6 text-center flex items-center justify-center py-3 text-base font-normal rounded text-white hover:shadow-lg bg-blue-50">Subscribe</button>
         </div>
-
-        <h3 className="font-medium text-lg">Send us an email or give us your information and we wil get back to you.</h3>
-        <div className="w-fit">
-          <Link data-aos="fade-right" to='/contact' className={`px-12 text-center flex items-center justify-center py-3 text-base font-normal rounded text-white hover:shadow-lg bg-blue-50`}>Contact</Link>
-        </div>
-
-        <ul className="flex gap-3 justify-between font-medium underline text-sm text-center">
-          <NavLink to='/about/disclaimer'>Disclaimer</NavLink>
-          <Link to='/about/privacy'>Privacy Policy</Link>
-          <Link to='/about/sustainability'>SFDR  Disclosures</Link>
-        </ul>
+        
       </div>
 
       <div className="flex-1 pt-10 px-[5%] flex lg:hidden flex-col items-start gap-10">
@@ -53,11 +48,6 @@ const Footer = () => {
 
       <div className="flex-1 pt-10 pb-16 px-[5%] flex lg:hidden flex-col items-start gap-8">
         
-
-        <h3 className="font-medium text-lg">Send us an email or give us your information and we wil get back to you.</h3>
-        <div className="w-fit">
-          <Link data-aos="fade-right" to='/contact' className={`px-12 text-center flex items-center justify-center py-3 text-base font-normal rounded text-white hover:shadow-lg bg-brown-50`}>Contact</Link>
-        </div>
 
         <ul className="flex gap-3 justify-between font-medium underline text-sm text-center">
           <NavLink to='/about/disclaimer'>Disclaimer</NavLink>
