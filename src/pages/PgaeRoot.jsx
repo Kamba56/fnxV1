63fb3d7fedb48407705b8aca6dac13ed1573d6f1
Toma John Musa <tomajohn316@yahoo.com>
2023-12-25 20:02:12 +0100

    Add vanta bg

--- src/pages/PgaeRoot.jsx
+++ src/pages/PgaeRoot.jsx
@@ -1,16 +1,43 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import { useEffect, useState } from "react";
-import logo from "../assets/images/blacklogo.svg"
+import { useEffect, useRef, useState } from "react";
+import logo from "../assets/images/blacklogo.svg";
+import GLOBE from "vanta/dist/vanta.globe.min";
+import * as THREE from "three";
 
 const PageRoot = () => {
   const [loading, setLoading] = useState(true);
+  const [vantaEffect, setVantaEffect] = useState(0);
+  const vantaRef = useRef(null);
+
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(
+        GLOBE({
+          el: vantaRef.current,
+          THREE: THREE,
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: false,
+          minHeight: 200.00,
+          minWidth: 200.00,
+          scale: 1.00,
+          scaleMobile: 1.00,
+          color: 0x174dff,
+          backgroundColor: 0xffffff
+        })
+      );
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, [vantaEffect]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        await new Promise(resolve => setTimeout(resolve, 500));
+        await new Promise(resolve => setTimeout(resolve, 1000));
         setLoading(false);
       } catch (error) {
         console.error('Error fetching data:', error);
@@ -25,7 +52,7 @@ const PageRoot = () => {
     <>
     {
       loading ? (
-        <div className="bg-white h-screen w-full flex justify-center items-center z-[100000000]">
+        <div ref={vantaRef} className="bg-white h-screen w-full flex justify-center items-center z-[100000000]">
           <figure className="w-24 animate-invert">
             <img src={logo} alt="" />
           </figure>
